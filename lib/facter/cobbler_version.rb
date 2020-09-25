c0076aa58497b28961799030dd9a946b0a6d0fad
Emir Imamagic <emir.imamagic@gmail.com>
2020-09-25 09:29:55 +0200

    Fix facter typo.

--- lib/facter/cobbler_version.rb
+++ lib/facter/cobbler_version.rb
@@ -1,7 +1,7 @@
 Facter.add(:cobbler_version) do
   setcode do
     if Facter::Util::Resolution.which('cobbler')
-      apache_version = Facter::Util::Resolution.exec('cobbler version 2>&1')
+      cobbler_version = Facter::Util::Resolution.exec('cobbler version 2>&1')
       Facter.debug "Matching cobbler '#{cobbler_version}'"
       %r{^Cobbler (\d+\.\d+\.\d+)}.match(cobbler_version)[1]
     end
