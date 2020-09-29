92c3fdbf21390f212e7cba1c4a01e217080d3a6b
Emir Imamagic <emir.imamagic@gmail.com>
2020-09-29 23:31:38 +0200

    Add http config template for Cobbler 3.

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,15 +17,17 @@ class cobbler::params {
       $distro_path            = '/distro'
       $apache_service         = 'httpd'
       if (versioncmp($cobbler_version,'3.0.0')>=0) {
-        $default_kickstart      = '/var/lib/cobbler/templates/default.ks'
-        $kickstarts_path        = '/var/lib/cobbler/templates'
-        $settings_template      = 'cobbler/settings.v3.erb'
-        $dhcp_template          = 'cobbler/dhcp.template.v3.erb'
+        $default_kickstart    = '/var/lib/cobbler/templates/default.ks'
+        $kickstarts_path      = '/var/lib/cobbler/templates'
+        $settings_template    = 'cobbler/settings.v3.erb'
+        $dhcp_template        = 'cobbler/dhcp.template.v3.erb'
+        $http_config_template = 'cobbler/cobbler.conf.v3.erb'
       } else {
-        $default_kickstart      = '/var/lib/cobbler/kickstarts/default.ks'
-        $kickstarts_path        = '/var/lib/cobbler/kickstarts'
-        $settings_template      = 'cobbler/settings.erb'
-        $dhcp_template          = 'cobbler/dhcp.template.erb'
+        $default_kickstart    = '/var/lib/cobbler/kickstarts/default.ks'
+        $kickstarts_path      = '/var/lib/cobbler/kickstarts'
+        $settings_template    = 'cobbler/settings.erb'
+        $dhcp_template        = 'cobbler/dhcp.template.erb'
+        $http_config_template = 'cobbler/cobbler.conf.erb'
       }
     }
     'Debian': {
@@ -42,15 +44,17 @@ class cobbler::params {
       $distro_path         = '/var/www/cobbler/ks_mirror'
       $apache_service      = 'apache2'
       if (versioncmp($cobbler_version,'3.0.0')>=0) {
-        $default_kickstart   = '/var/lib/cobbler/templates/ubuntu-server.preseed'
-        $kickstarts_path     = '/var/lib/cobbler/templates'
-        $settings_template   = 'cobbler/settings.v3.erb'
-        $dhcp_template       = 'cobbler/dhcp.template.v3.erb'
+        $default_kickstart    = '/var/lib/cobbler/templates/ubuntu-server.preseed'
+        $kickstarts_path      = '/var/lib/cobbler/templates'
+        $settings_template    = 'cobbler/settings.v3.erb'
+        $dhcp_template        = 'cobbler/dhcp.template.v3.erb'
+        $http_config_template = 'cobbler/cobbler.conf.v3.erb'
       } else {
-        $default_kickstart   = '/var/lib/cobbler/kickstarts/ubuntu-server.preseed'
-        $kickstarts_path     = '/var/lib/cobbler/kickstarts'
-        $settings_template   = 'cobbler/settings.erb'
-        $dhcp_template       = 'cobbler/dhcp.template.erb'
+        $default_kickstart    = '/var/lib/cobbler/kickstarts/ubuntu-server.preseed'
+        $kickstarts_path      = '/var/lib/cobbler/kickstarts'
+        $settings_template    = 'cobbler/settings.erb'
+        $dhcp_template        = 'cobbler/dhcp.template.erb'
+        $http_config_template = 'cobbler/cobbler.conf.erb'
       }
     }
     default: {
