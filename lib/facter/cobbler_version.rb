ae4f9024356458ae58a8b54419bc2f82cba2b7c5
Ante Jurjevic <ante.jurjevic@srce.hr>
2021-09-08 13:45:12 +0200

    Add

--- lib/facter/cobbler_version.rb
+++ lib/facter/cobbler_version.rb
@@ -1,7 +1,7 @@
 Facter.add(:cobbler_version) do
   setcode do
     if Facter::Util::Resolution.which('cobbler')
-      cobbler_version = Facter::Util::Resolution.exec('cobbler version 2>&1')
+      cobbler_version = Facter::Util::Resolution.exec('cat /etc/cobbler/version | grep "version " | tr -d "version = "')
       Facter.debug "Matching cobbler '#{cobbler_version}'"
       %r{^Cobbler (\d+\.\d+\.\d+)}.match(cobbler_version)[1]
     end
